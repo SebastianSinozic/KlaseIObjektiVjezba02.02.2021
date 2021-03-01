973ebe8cda24a79daa35a98ea5960add0c525af6
SebastianSinozic <55134663+SebastianSinozic@users.noreply.github.com>
2021-03-01 20:04:59 +0100

    Add files via upload

---
+++ sinozicSebastianKlasaX/sinozicSebastianKlasaX/KlasaX.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sinozicSebastianKlasaX
+{
+    class KlasaX
+    {
+        int[] arr = new int[100];
+        int index = 0;
+    }
+}
---
+++ sinozicSebastianKlasaX/sinozicSebastianKlasaX/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace sinozicSebastianKlasaX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KlasaX a = new KlasaX();
+            for (int i = 0; i < 100; i++) a.arr[i] = i;
+            a.index = 50;
+            Console.WriteLine(a.arr[a.index]);
+            Console.ReadKey();
+        }
+    }
+}
